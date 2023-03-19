2a6029f0182c1f421b18bc324d41cb42830d5f30
AmmarFarid10 <91678027+AmmarFarid10@users.noreply.github.com>
2023-03-20 04:06:49 +0500

    Update reviewController.go

--- controllers/reviewController.go
+++ controllers/reviewController.go
@@ -1,72 +1,59 @@
-package controllers
-
-import (
-	"context"
-	"net/http"
-	"shive-app/database"
-	helper "shive-app/helpers"
-	"shive-app/models"
-	"time"
-
-	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-)
-
-var reviewCollection *mongo.Collection = database.OpenCollection(database.Client, "review")
-
-//Add a new review
 func AddAReview() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		if err := helper.VerifyUserType(c, "USER"); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		var review models.Reviews
-		defer cancel()
+        return func(c *gin.Context) {
+                if err := helper.CheckUserType(c, "USER"); err != nil {
+                        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+                        return
+                }
+                ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+                var review models.Reviews
+                defer cancel()
 
-		//validate the request body
-		if err := c.BindJSON(&review); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"Status":  http.StatusBadRequest,
-				"Message": "error",
-				"Data":    map[string]interface{}{"data": err.Error()}})
-			return
-		}
+                //validate the request body
+                if err := c.BindJSON(&review); err != nil {
+                        c.JSON(http.StatusBadRequest, gin.H{
+                                "Status":  http.StatusBadRequest,
+                                "Message": "error",
+                                "Data":    map[string]interface{}{"data": err.Error()}})
+                        return
+                }
+                //use the validator library to validate required fields
+                if validationErr := validate.Struct(&review); validationErr != nil {
+                        c.JSON(http.StatusBadRequest, gin.H{
+                                "Status":  http.StatusBadRequest,
+                                "Message": "error",
+                                "Data":    map[string]interface{}{"data": validationErr.Error()}})
+                        return
+                }
 
-		//use the validator library to validate required fields
-		if validationErr := validate.Struct(&review); validationErr != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"Status":  http.StatusBadRequest,
-				"Message": "error",
-				"Data":    map[string]interface{}{"data": validationErr.Error()}})
-			return
-		}
+                newReview := models.Reviews{
+                        Id:          primitive.NewObjectID(),
+                        Movie_id:    review.Movie_id,
+                        Reviewer_id: review.Reviewer_id,
+                        Review:      review.Review,
+			Review_id: review.Review_id,
+                }
 
-		newReview := models.Reviews{
-			Id:          primitive.NewObjectID(),
-			Movie_id:    review.Movie_id,
-			Review_id:   review.Review_id,
-			Reviewer_id: review.Reviewer_id,
-			Review:      review.Review,
-			Created_at:  time.Now(),
-			Updated_at:  time.Now(),
-		}
+                result, err := reviewCollection.InsertOne(ctx, newReview)
 
-		result, err := reviewCollection.InsertOne(ctx, newReview)
+                if err != nil {
+                        c.JSON(http.StatusBadRequest, gin.H{
+                                "Status":  http.StatusBadRequest,
+                                "Message": "error",
+                                "Data":    map[string]interface{}{"data": err.Error()}})
+                        return
+                }
 
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"Status":  http.StatusBadRequest,
-				"Message": "error",
-				"Data":    map[string]interface{}{"data": err.Error()}})
-			return
-		}
+                if err != nil {
+                        c.JSON(http.StatusInternalServerError, gin.H{
+                                "Status":  http.StatusInternalServerError,
+                                "Message": "error",
+                                "Data":    map[string]interface{}{"data": err.Error()}})
+                        return
+                }
 
-		c.JSON(http.StatusCreated, gin.H{
-			"Status":  http.StatusCreated,
-			"Message": "success",
-			"Data":    map[string]interface{}{"data": result}})
-	}
+                c.JSON(http.StatusCreated, gin.H{
+                        "Status":  http.StatusCreated,
+                        "Message": "success",
+                        "Data":    map[string]interface{}{"data": result}})
+        }
 }
