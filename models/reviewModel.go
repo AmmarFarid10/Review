0402455e10381ab061f23d0acfb483666d667922
AmmarFarid10 <91678027+AmmarFarid10@users.noreply.github.com>
2023-03-20 03:44:06 +0500

    Update reviewModel.go

--- models/reviewModel.go
+++ models/reviewModel.go
@@ -9,7 +9,7 @@ import (
 type Reviews struct {
 	Id          primitive.ObjectID `bson:"_id"`
 	Movie_id    int            `json:"movie_id"`
-	Reviewer_id primitive.ObjectID `bson:"_id"`
+	Reviewer_id *string            `json:"reviewer_id"`
 	Review_id   int  	`json:"review_id"`
 	Review      *string            `json:"review" validate:"required"`
 	Created_at  time.Time          `json:"created_at"`
